cf099826f1fc1a0811ce802116146dd2bd8452a9
GalaxyDream <xinsongdu@ufl.edu>
2019-03-07 15:54:26 -0500

    add flexibility to xcms-faahKO.R

---
+++ xcms_R/xcms-faahKO.R
@@ -0,0 +1,124 @@
+# LCMS data preprocessing and analysis with xcms
+
+# 26 June 2018
+# Package
+# xcms 3.3.2
+# 
+# Package: xcms
+# Authors: Johannes Rainer
+# Modified: 2018-05-28 16:46:20
+# Compiled: Tue Jun 26 18:48:06 2018
+
+# Notes: https://www.ebi.ac.uk/training/online/course/lcms-data-analysis-xcms-and-metfrag-phenomenal
+
+# Data
+# https://bioconductor.org/packages/release/data/experiment/html/faahKO.html
+
+# BOOT into docker container
+# docker run -ti bioconductor/release_metabolomics2 R
+
+library(xcms)
+library(faahKO)
+library(RColorBrewer)
+library(pander)
+library(magrittr)
+
+## Data Import
+## ------------
+
+## Get the full path to the CDF files
+files <- dir(system.file("cdf", package = "faahKO"), full.names = TRUE,
+            recursive = TRUE)
+
+# add flaxibility to sample_group and sample_name to the original file
+sg = c()
+sn = c()
+for (name in strsplit(files, "/")){
+    sg = append(sg, name[length(name) - 1])
+    sn = append(sn, name[length(name)])
+}
+
+## Create a phenodata data.frame
+    # modified sample_name and sample_group of the original file
+pd <- data.frame(sample_name = sn,
+                 sample_group = sg,
+                 stringsAsFactors = FALSE) 
+
+
+## load the raw data as an OnDiskMSnExp object using the readMSData method 
+## from the MSnbase package.
+raw_data <- readMSData(files = files, pdata = new("NAnnotatedDataFrame", pd),
+                       mode = "onDisk") 
+  # The OnDiskMSnExp object contains general information about the number of spectra, 
+  # retention times, the measured total ion current etc, but does not contain the 
+  # full raw data (i.e. the m/z and intensity values from each measured spectrum).
+
+## Initial data inspection
+## -----------------------
+
+# extract the retention time values from the object.
+head(rtime(raw_data)) 
+
+# returns a numeric vector that provides the mapping of the values to 
+# the originating file
+mzs <- mz(raw_data)
+
+## Split the list by file
+mzs_by_file <- split(mzs, f = fromFile(raw_data))
+length(mzs_by_file) 
+
+## Get the base peak chromatograms. This reads data from the files.
+bpis <- chromatogram(raw_data, aggregationFun = "max")
+## Define colors for the two groups
+group_colors <- brewer.pal(3, "Set1")[1:2]
+names(group_colors) <- c("KO", "WT")
+
+## Plot all chromatograms.
+plot(bpis, col = group_colors[raw_data$sample_group])
+
+## note: create a total ion chromatogram we could set aggregationFun to sum.
+
+# The chromatogram method returned a Chromatograms object that organizes individual
+# Chromatogram objects (which in fact contain the chromatographic data) in a 
+# two-dimensional array: columns represent samples and rows (optionally) m/z 
+# and/or retention time ranges. Below we extract the chromatogram of the first 
+# sample and access its retention time and intensity values.
+
+bpi_1 <- bpis[1, 1]
+head(rtime(bpi_1))
+head(intensity(bpi_1)) 
+
+# Below we create boxplots representing the distribution of total ion 
+# currents per file. Such plots can be very useful to spot problematic or 
+# failing MS runs.
+
+## Get the total ion current by file
+tc <- split(tic(raw_data), f = fromFile(raw_data))
+boxplot(tc, col = group_colors[raw_data$sample_group],
+        ylab = "intensity", main = "Total ion current")
+
+## Chromatographic peak detection
+
+#To evaluate the typical chromatographic peak width we plot the EIC for one peak.
+
+## Define the rt and m/z range of the peak area
+rtr <- c(2700, 2900)
+mzr <- c(334.9, 335.1)
+## extract the chromatogram
+chr_raw <- chromatogram(raw_data, mz = mzr, rt = rtr)
+plot(chr_raw, col = group_colors[chr_raw$sample_group]) 
+
+raw_data %>%
+  filterRt(rt = rtr) %>%
+  filterMz(mz = mzr) %>%
+  plot(type = "XIC") 
+
+## Below we perform the chromatographic peak detection using the 
+## findChromPeaks method. The submitted parameter object defines which 
+## algorithm will be used and allows to define the settings for this algorithm. 
+## Note that we set the argument noise to 1000 to slightly speed up the analysis
+## by considering only signals with a value larger than 1000 in the peak 
+## detection step.
+
+cwp <- CentWaveParam(peakwidth = c(30, 80), noise = 1000)
+xdata <- findChromPeaks(raw_data, param = cwp) 
