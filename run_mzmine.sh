d41d3eb8ae820b3076b948f5396647ace13f1f6e
GalaxyDream <xinsongdu@ufl.edu>
2019-09-24 23:41:38 -0400

    clean outdated files

--- run_mzmine.sh
+++
@@ -1,4 +0,0 @@
-nextflow batchfile_generator.nf &&
-./docker/main/MZmine-2.38/startMZmine_MacOSX.command $(pwd)/results/config.xml
-
-#/Users/xinsongdu/mnt/projects/metabolomics_data_processing/results/config.xml
