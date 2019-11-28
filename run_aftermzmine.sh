51b69f98889a41065f20579e457b41977581859f
GalaxyDream <xinsongdu@ufl.edu>
2019-11-28 11:14:56 -0500

    add run_aftermzmine

---
+++ run_aftermzmine.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=nf_r_processing_sample
+#SBATCH --mail-type=ALL #Changed ALL to NONE
+#SBATCH --mail-user=xinsongdu@ufl.edu
+#SBATCH --ntasks=10
+#SBATCH --mem=20gb
+#SBATCH --account=djlemas
+#SBATCH --qos=djlemas-b
+#SBATCH --time=10:00:00
+#SBATCH --output=./logs/nf_r_postprocessing_%j.log
+pwd; hostname; date
+
+# load modules
+ml R
+ml nextflow/18.10.1
+ml singularity
+SINGULARITYENV_TMPDIR=$(pwd)/tmp
+export SINGULARITYENV_TMPDIR
+TMPDIR=$(pwd)/tmp
+# XDG_RUNTIME_DIR=$(pwd)/singularity_cache
+export TMPDIR
+
+nextflow run_R_postprocessing.nf -with-singularity docker://galaxydream/mzmine_oldversion
+date
