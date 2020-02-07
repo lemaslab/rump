593851617642baa50a33b5516cc5164c124c8eef
GalaxyDream <xinsongdu@ufl.edu>
2020-02-07 14:09:45 -0500

    debug

--- run_HumanVSBovine.sh
+++ run_HumanVSBovine.sh
@@ -12,13 +12,12 @@ pwd; hostname; date
 
 # load modules
 ml R
-ml nextflow/18.10.1
+ml nextflow/19.07.0
 ml singularity
 SINGULARITYENV_TMPDIR=$(pwd)/tmp
 export SINGULARITYENV_TMPDIR
 TMPDIR=$(pwd)/tmp
 export TMPDIR
-unset XDG_RUNTIME_DIR
 
 nextflow run_all.nf --pos_mzmine_peak_output pos_data.csv --neg_mzmine_peak_output neg_data.csv --input_dir_pos /ufrc/djlemas/xinsongdu/jupyter_notebook/data/metabolomics/Human_Bovine/mzXML/POS_HumanVSBovine --input_dir_neg /ufrc/djlemas/xinsongdu/jupyter_notebook/data/metabolomics/Human_Bovine/mzXML/NEG_HumanVSBovine --POS_design_path ./data/pos_design_HumanVSBovine.csv --NEG_design_path ./data/neg_design_HumanVSBovine.csv -with-singularity docker://galaxydream/metabolomics_pipeline
 date
